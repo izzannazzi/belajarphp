e9711c34e048d3d01209eee035113f09b91450db
izzannazzi <izzannazzi10@gmail.com>
2022-02-13 21:15:34 +0700

    Operator dalam PHP 3

--- materi.php
+++ materi.php
@@ -1,7 +1,27 @@
 <?php 
-  $nama_depan = "Tauhid";
-  $nama_belakang = "Izzan";
+ $x = 10;
+ $x += 20; 
 
-  echo $nama_depan . " " . $nama_belakang;
-    
+ echo "$x  </br>";
+ 
+ $y = 5;
+ $y -= 2;
+
+ echo "$y </br>"; 
+
+ $a = 10;
+ $a /= 2;
+
+ echo "$a </br>"; 
+
+ $b = 5;
+ $b %= 10;
+
+ echo "$b </br>";
+
+ $c = 25;
+ $c .= 10;
+
+ echo $c;
+
 ?>
