940a0bf7497f7ab4a90e324daedf7124417ff69e
izzannazzi <izzannazzi10@gmail.com>
2022-02-13 20:46:32 +0700

    Konsep Interpolasi

--- materi.php
+++ materi.php
@@ -1,27 +1,8 @@
 <?php 
     
-    // variabel dan tipe data
-    // variabel = digunakan untuk menyimpan data
-    // Aturan Variabel 'tidak boleh diawali dengan angka,
-    // tapi boleh mengandung angka'
-    
     $nama = "Izzan";
+
+    echo "Hallo, Selamat Datang $nama </br>"; #petik dua
+    echo 'Hallo, Selamat Datang $nama'; #petik satu
     
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-        Belajar PHP
-  </title>
-</head>
-<body>
-
-<h1>Hello, Selamat Datang <?php echo $nama; ?></h1>
-
-</body>
-</html>
